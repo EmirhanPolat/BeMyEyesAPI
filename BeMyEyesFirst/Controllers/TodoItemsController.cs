a3c690aa8d6d0baedb9665ec38ebd4d95116cb2b
EmirhanPolat <58660645+EmirhanPolat@users.noreply.github.com>
2023-10-20 23:02:28 +0300

    Add project files.

---
+++ BeMyEyesFirst/Controllers/TodoItemsController.cs
@@ -0,0 +1,175 @@
+﻿using Azure;
+using Azure.AI.Vision.Common;
+using Azure.AI.Vision.ImageAnalysis;
+using BeMyEyesFirst.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using NuGet.Protocol;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoItemsController : ControllerBase
+{
+    private readonly TodoContext _context;
+
+    private readonly DescribeImageSampleService describeImageSampleService;
+
+    public static string subscriptionKey = Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
+    public static string endpoint = Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
+
+    public TodoItemsController(TodoContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/TodoItems
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
+    {
+        return await _context.TodoItems
+            .Select(x => ItemToDTO(x))
+            .ToListAsync();
+    }
+
+    // GET: api/TodoItems/5
+    // <snippet_GetByID>
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+
+        await DescribeImageSampleService.RunAsync(endpoint, subscriptionKey);
+        Console.WriteLine("\nPress ENTER to exit.");
+        Console.ReadLine();
+
+        ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+        
+        var response = client.AnalyzeImageAsync("https://png2jpg.com/images/png2jpg/icon.png");
+        Console.WriteLine(response.Result.ToJson());
+
+        using var imageSource = VisionSource.FromUrl(
+    new Uri("https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"));
+
+        var analysisOptions = new ImageAnalysisOptions()
+        {
+            Features =
+          ImageAnalysisFeature.CropSuggestions
+        | ImageAnalysisFeature.Caption
+        | ImageAnalysisFeature.DenseCaptions
+        | ImageAnalysisFeature.Objects
+        | ImageAnalysisFeature.People
+        | ImageAnalysisFeature.Text
+        | ImageAnalysisFeature.Tags
+        };
+        var serviceOptions = new VisionServiceOptions(endpoint, new AzureKeyCredential(subscriptionKey));
+
+        using var analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
+
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        return ItemToDTO(todoItem);
+    }
+
+    public static ComputerVisionClient Authenticate(string endpoint, string key)
+    {
+        ComputerVisionClient client =
+          new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+          { Endpoint = endpoint };
+        return client;
+    }
+    // </snippet_GetByID>
+
+    // PUT: api/TodoItems/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    // <snippet_Update>
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoDTO)
+    {
+        if (id != todoDTO.Id)
+        {
+            return BadRequest();
+        }
+
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        todoItem.Name = todoDTO.Name;
+        todoItem.IsComplete = todoDTO.IsComplete;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+    // </snippet_Update>
+
+    // POST: api/TodoItems
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    // <snippet_Create>
+    [HttpPost]
+    public async Task<ActionResult<TodoItemDTO>> PostTodoItem(TodoItemDTO todoDTO)
+    {
+        var todoItem = new TodoItem
+        {
+            IsComplete = todoDTO.IsComplete,
+            Name = todoDTO.Name
+        };
+
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetTodoItem),
+            new { id = todoItem.Id },
+            ItemToDTO(todoItem));
+    }
+    // </snippet_Create>
+
+    // DELETE: api/TodoItems/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(long id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        _context.TodoItems.Remove(todoItem);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool TodoItemExists(long id)
+    {
+        return _context.TodoItems.Any(e => e.Id == id);
+    }
+
+    private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+       new TodoItemDTO
+       {
+           Id = todoItem.Id,
+           Name = todoItem.Name,
+           IsComplete = todoItem.IsComplete
+       };
+}
---
+++ BeMyEyesFirst/Services/DescribeImageSampleService.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace BeMyEyesFirst.Services
+{
+    public class DescribeImageSampleService
+    {
+        public static async Task RunAsync(string endpoint, string key)
+        {
+            Console.WriteLine("Describe an image:");
+            // See this repo's readme.md for info on how to get these images. Or, set the path to any appropriate image on your machine.
+            //string imageFilePath = @"Images\objects.jpg";
+            string remoteImageUrl = "https://github.com/Azure-Samples/cognitive-services-sample-data-files/raw/master/ComputerVision/Images/celebrities.jpg";
+
+            //await DescribeImageFromStreamAsync(imageFilePath, endpoint, key);
+            await DescribeImageFromUrlAsync(remoteImageUrl, endpoint, key);
+        }
+
+        //static async Task DescribeImageFromStreamAsync(string imageFilePath, string endpoint, string subscriptionKey)
+        //{
+        //    if (!File.Exists(imageFilePath))
+        //    {
+        //        Console.WriteLine("\nInvalid file path");
+        //        return;
+        //    }
+        //    try
+        //    {
+        //        HttpClient client = new HttpClient();
+
+        //        // Request headers.
+        //        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+        //        string uri = $"{endpoint}/vision/v2.0/describe";
+        //        // Read the contents of the specified local image into a byte array.
+        //        byte[] byteData = GetImageAsByteArray(imageFilePath);
+        //        // Add the byte array as an octet stream to the request body.
+        //        using (ByteArrayContent content = new ByteArrayContent(byteData))
+        //        {
+        //            // This example uses the "application/octet-stream" content type.
+        //            // The other content types you can use are "application/json" and "multipart/form-data".
+        //            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+        //            // Asynchronously call the REST API method.
+        //            HttpResponseMessage response = await client.PostAsync(uri, content);
+        //            // Asynchronously get the JSON response.
+        //            string contentString = await response.Content.ReadAsStringAsync();
+        //            // Display the JSON response.
+        //            Console.WriteLine("\nResponse:\n\n{0}\n", JToken.Parse(contentString).ToString());
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("\n" + e.Message);
+        //    }
+        //}
+
+        //static byte[] GetImageAsByteArray(string imageFilePath)
+        //{
+        //    // Open a read-only file stream for the specified file.
+        //    using (FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+        //    {
+        //        // Read the file's contents into a byte array.
+        //        BinaryReader binaryReader = new BinaryReader(fileStream);
+        //        return binaryReader.ReadBytes((int)fileStream.Length);
+        //    }
+        //}
+
+        static async Task DescribeImageFromUrlAsync(string imageUrl, string endpoint, string subscriptionKey)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine("\nInvalid remote image url:\n{0} \n", imageUrl);
+                return;
+            }
+            try
+            {
+                HttpClient client = new HttpClient();
+                // Request headers
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                string uri = $"{endpoint}/vision/v2.0/describe";
+
+                string requestBody = " {\"url\":\"" + imageUrl + "\"}";
+                var content = new StringContent(requestBody);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // Post the request and display the result
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+                string contentString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("\nResponse:\n\n{0}\n", JToken.Parse(contentString).ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+    }
+}
