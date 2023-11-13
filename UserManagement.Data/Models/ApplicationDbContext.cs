f4f61a9711eccede60a5d05a0a66d8c6fa03a524
EmirhanPolat <58660645+EmirhanPolat@users.noreply.github.com>
2023-11-13 15:01:29 +0300

    Auth and separation done

---
+++ UserManagement.Data/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagement.Data.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<User, UserRoles, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
